98aa4983d8aa46f08a501680201a7f0da34e6423
guilherme <guilhermenatal47@gmail.com>
2022-11-07 11:46:21 -0300

    Template for a TypeORM express

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express'
-import { getUsersData } from '../Controllers/csvDatabase'
-
+import PhotosController from '../Controllers/PhotosController';
 
 const routes = Router()
 
-routes.get('/', getUsersData);
+routes.get('/', (req, res) => {
+  return res.send('Hello world').status(200)
+});
 
 routes.get('/healt', (req, res) => {
   console.log('app is working')
   return res.send('app is working').status(200)
 })
+
+routes.post('/photo', PhotosController.create)
 
 export default routes
